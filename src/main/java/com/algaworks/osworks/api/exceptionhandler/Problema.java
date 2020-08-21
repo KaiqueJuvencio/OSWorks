78d01339edb685ec42bd85bde1827bf06ccd7e5d
Kaique <kaiquejuvencio@hotmail.com>
2020-08-21 00:29:19 -0300

    Incrementando respostas de erros

--- src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
@@ -1,11 +1,36 @@
 package com.algaworks.osworks.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Problema {
 	private Integer status;
 	private LocalDateTime dataHora;
 	private String titulo;
+	private List<Campo> campos;
+	
+	public static class Campo{
+		private String nome;
+		private String mensagem;
+		public String getNome() {
+			return nome;
+		}
+		public Campo(String nome, String mensagem) {
+			super();
+			this.nome = nome;
+			this.mensagem = mensagem;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getMensagem() {
+			return mensagem;
+		}
+		public void setMensagem(String mensagem) {
+			this.mensagem = mensagem;
+		}
+	}
+	
 	public Integer getStatus() {
 		return status;
 	}
@@ -24,4 +49,11 @@ public class Problema {
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+	public List<Campo> getCampos() {
+		return campos;
+	}
+	public void setCampos(List<Campo> campos) {
+		this.campos = campos;
+	}
+	
 }
